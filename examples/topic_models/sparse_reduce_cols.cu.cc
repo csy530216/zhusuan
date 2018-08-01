47b553aed3bb5ae02053c178e257c663fb915633
milesxu <milesxu@outlook.com>
2018-08-01 10:08:59 +0800

    sparse reduce cols 0.1 ver

--- examples/topic_models/sparse_reduce_cols.cu.cc
+++ examples/topic_models/sparse_reduce_cols.cu.cc
@@ -10,13 +10,13 @@ const int sum_len = 256;
 const int work_per_thread = 4;
 
 __global__ void SparseReduceColsKernel(int numvals, const float *values,
-                                       const float *indices, const int64 *shape,float *sum_vec)
+                                       const float *indices, const int64 *shape, float *sum_vec)
 {
     __shared__ float sum[sum_len];
     auto threadId = blockIdx.x * blockDim.x + threadIdx.x;
     auto block_start_thread = threadId & 31;
     auto start_idx = threadId * work_per_thread;
-    auto end_idx = min(start_idx + work_per_thread, numvals * 2 - 1);
+    auto end_idx = min(start_idx + work_per_thread, numvals * 2 - 2);
     auto start_share = indices[block_start_thread * work_per_thread * 2];
     auto id = -1;
     auto val = 0.0f;
@@ -42,12 +42,26 @@ __global__ void SparseReduceColsKernel(int numvals, const float *values,
         }
     }
     audo share_id = id - start_share;
-    atomicAdd(&sum + share_id, val);
+    atomicAdd(sum + share_id, val);
+    auto global_start = indices[block_start_thread * 2];
+    auto global_end = indices[blockk_start_thread + sum_len * 2];
+    for (auto i = threadIdx.x; i < global_end - global_start; i += blockDim.x)
+    {
+        auto index = global_start + i;
+        if (i == 0 || index == glbobal_end - 1)
+            atomicAdd(sum_vec + index, sum[i]);
+        else
+            sum_vec[index] = sum[i];
+    }
 }
 
 void SparseReduceFunctor<GPUDevice>::operator()(GPUDevice &d, int numvals,
                                                 const float *values, const int64 *indices, const int64 *shape, float *sum_vec)
 {
+    auto threads_per_block = sum_len / work_per_thread;
+    auto numblocks = (numvals + sum_len - 1) / sum_len;
+    SparseReduceColsKernel<<<numblocks, threads_per_block>>>(numvals, values,
+                                                             indices, shape, sum_vec);
 }
 
 #endif //GOOGLE_CUDA

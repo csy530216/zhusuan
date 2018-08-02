d29cf7ab143ec65b7e1df8e6399352c7c505424b
Ming Xu <mrxuming@gmail.com>
2018-08-02 15:47:55 +0800

    try to write gradient of src

--- examples/topic_models/src.py
+++ examples/topic_models/src.py
@@ -1,7 +1,15 @@
 import tensorflow as tf
+from tensorflow.python.framework import ops
 
 src_module = tf.load_op_library('./sparse_reduce_cols.so')
 
 
 def src(values, indices, shape):
     return src_module.sparse_reduce_cols(values, indices, shape)
+
+@ops.RegisterGradient("SparseReduceCols")
+def _sparse_reduce_cols_grad(op, grad):
+    val = op.inputs[0]
+    idx = op.inputs[1]
+    shp = op.inputs[2]
+    

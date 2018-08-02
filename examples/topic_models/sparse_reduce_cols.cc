413c52e4c90021fcb1c4c3850aac0e3d270babc9
milesxu <milesxu@outlook.com>
2018-08-03 00:02:11 +0800

    correct HostMemory

--- examples/topic_models/sparse_reduce_cols.cc
+++ examples/topic_models/sparse_reduce_cols.cc
@@ -1,5 +1,6 @@
 #include "sparse_reduce_cols.h"
-#include "tensorflow/core/framework/shape_inference.h"
+//#include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/framework/tensor_shape.h"
 
 using namespace tensorflow; // NOLINT(build/namespaces)
 
@@ -23,8 +24,7 @@ template <typename Device>
 class SparseReduceColsOp : public OpKernel
 {
   public:
-    explicit SparseReduceColsOp(OpKernelConstruction *context)
-        : OpKernel(context) {}
+    explicit SparseReduceColsOp(OpKernelConstruction *c) : OpKernel(c) {}
 
     void Compute(OpKernelContext *context) override
     {
@@ -33,15 +33,23 @@ class SparseReduceColsOp : public OpKernel
         const Tensor &shape_input = context->input(2);
         const int64 num_values = vals.dim_size(0);
         std::cout << "vals num: " << num_values << " "
-                  << vals.flat<float>().size() << ""
+                  << vals.flat<float>().size() << " "
                   << vals.NumElements() << std::endl;
+        std::cout << shape_input.NumElements() << std::endl;
 
+        //std::cout << "the shape: " << std::endl;
         auto vec = shape_input.flat<int64>();
+        //auto vec = shape_input.vec<int64>();
         //printf("the shape: %d", vec(0));
-        std::cout << "the shape: " << vec << std::endl;
+        //std::cout << vec(0) << std::endl;
+        std::cout << "flat complete. the shape: " << std::endl;
+        //std::cout << vec.data() << std::endl;
+        std::cout << vec(0) << std::endl;
         TensorShape shape;
         OP_REQUIRES_OK(context,
-                       TensorShapeUtils::MakeShape(vec.data(), 1, &shape));
+                       TensorShapeUtils::MakeShape(vec.data(), 1,
+                                                   &shape));
+        std::cout << "shape creation complete." << std::endl;
 
         Tensor *output = NULL;
         OP_REQUIRES_OK(context,
@@ -59,10 +67,12 @@ class SparseReduceColsOp : public OpKernel
 
 // Register the GPU kernels.
 #ifdef GOOGLE_CUDA
-#define REGISTER_GPU()                                                   \
-    /* It's recommended to add the code below, but not essential. */     \
-    extern template struct SparseReduceColsFunctor<GPUDevice>;           \
-    REGISTER_KERNEL_BUILDER(Name("SparseReduceCols").Device(DEVICE_GPU), \
+#define REGISTER_GPU()                                               \
+    /* It's recommended to add the code below, but not essential. */ \
+    extern template struct SparseReduceColsFunctor<GPUDevice>;       \
+    REGISTER_KERNEL_BUILDER(Name("SparseReduceCols")                 \
+                                .Device(DEVICE_GPU)                  \
+                                .HostMemory("shape"),                \
                             SparseReduceColsOp<GPUDevice>);
 REGISTER_GPU();
 #endif // GOOGLE_CUDA

6e07a0893e299782e9ac74ca7101c1c23db7e3ff
milesxu <milesxu@outlook.com>
2018-08-15 20:14:33 +0800

    add syncs

--- examples/topic_models/sparse_reduce_cols.cu.cc
+++ examples/topic_models/sparse_reduce_cols.cu.cc
@@ -2,7 +2,7 @@
 #define EIGEN_USE_GPU
 #include "sparse_reduce_cols.h"
 //#include "tensorflow/core/util/cuda_kernel_helper.h"
-#include <iostream>
+//#include <iostream>
 //#include <stdio.h>
 
 using namespace tensorflow;
@@ -18,6 +18,7 @@ __global__ void SparseReduceColsKernel(int numvals, const float *values,
     __shared__ float sum[sum_len];
     for (auto i = threadIdx.x; i < sum_len; i += blockDim.x)
         sum[i] = 0.0f;
+    __syncthreads();
     auto block_start_offset = blockIdx.x * sum_len;
     auto block_end_offset = min(numvals, block_start_offset + sum_len);
     auto block_start_idx = indices[block_start_offset * 2];
@@ -57,6 +58,7 @@ __global__ void SparseReduceColsKernel(int numvals, const float *values,
         auto id_offset = id - block_start_idx;
         atomicAdd(sum + id_offset, val);
     }
+    __syncthreads();
     auto bound = block_end_idx - block_start_idx + 1;
     for (auto i = threadIdx.x; i < bound; i += blockDim.x)
     {
@@ -71,10 +73,23 @@ __global__ void SparseReduceColsKernel(int numvals, const float *values,
     }
 }
 
+__global__ void zero(const long long num, float *sum)
+{
+    auto threadId = blockIdx.x * blockDim.x + threadIdx.x;
+    auto stride = gridDim.x * blockDim.x;
+    for (auto i = threadId; i < num; i += stride)
+    {
+        sum[i] = 0.0f;
+    }
+}
+
 template <typename GPUDevice>
 void SparseReduceColsFunctor<GPUDevice>::operator()(const GPUDevice &d,
                                                     int numvals, const float *values, const long long *indices, const long long *shape, float *sum_vec)
 {
+    auto tpb = 64;
+    auto nb = (shape[0] + tpb - 1) / tpb;
+    zero<<<nb, tpb>>>(shape[0], sum_vec);
     auto threads_per_block = sum_len / work_per_thread;
     auto numblocks = (numvals + sum_len - 1) / sum_len;
     //std::cout << "cuda kernel begin..." << std::endl;
@@ -83,8 +98,8 @@ void SparseReduceColsFunctor<GPUDevice>::operator()(const GPUDevice &d,
     {
         temp.push_back(indices[i * 2]);
     }*/
-    std::cout << "in sparse reduce cols: " << shape[0] << " "
-              << shape[1] << std::endl;
+    /*std::cout << "in sparse reduce cols: " << shape[0] << " "
+              << shape[1] << std::endl;*/
     SparseReduceColsKernel<<<numblocks, threads_per_block>>>(numvals, values,
                                                              indices, shape, sum_vec);
     //std::cout << "cuda kernel src complete" << std::endl;
